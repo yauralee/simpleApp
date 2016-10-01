1f7a0c6ca61b2e69ec6e86728c5f5ae48fd46d37
Li Yu <yu.li@rea-group.com>
2016-10-01 21:18:42 +0800

    import third party jar

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by twcn on 10/1/16.
+ */
+public class Greeter {
+    public String sayHello(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package hello;
+
+
+/**
+ * Created by twcn on 10/1/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("current time is :" + currentTime);
+        Greeter greeter = new Greeter();
+        greeter.sayHello();
+    }
+}
